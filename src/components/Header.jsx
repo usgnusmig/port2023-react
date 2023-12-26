546855581c851f15d298284938aecffcc992a52e
usgnusmig <usgnusmig@gmail.com>
2023-12-26 22:55:39 +0100

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headerNav = [
+  {
+    title: "intro",
+    url: "#intro",
+  },
+  {
+    title: "skill",
+    url: "#skill",
+  },
+  {
+    title: "site",
+    url: "#site",
+  },
+  {
+    title: "portfolio",
+    url: "#port",
+  },
+  {
+    title: "contact",
+    url: "#contact",
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  };
+
+  return (
+    <header id="header" role="banner">
+      <div className="header__inner">
+        <div className="header_logo">
+          <a href="/">
+            portfolio<em>vite</em>
+          </a>
+        </div>
+        <nav className="header_nav" role="navigation" aria-label="메인 메뉴">
+          <ul>
+            {headerNav.map((nav, key) => (
+              <li key={key}>
+                <a href={nav.url}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div
+          className="header__nav__mobile"
+          id="headerToggle"
+          aria-controls="primary-menu"
+          aria-expanded="false"
+          role="button"
+          tabindex="0"
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
